1ea7a7303ed85cf2a5bd3eed589f548b0601e67f
seungyeon <seungyeon@localhost>
2021-04-27 13:22:37 +0900

    mission: export monthly calendar

---
+++ src/calendar4.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class calendar4 {
+	private final static String PROMPT = "> ";
+	private static final int[] Maxdate = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static void perMonthCalendar(int month) {
+		System.out.println(" 일  월  화  수  목  금  토");
+		System.out.println("--------------------");
+		for(int i=0; i < Maxdate[month-1]; i++) {
+			if(i>1 && i%7 == 0) {
+				System.out.println("");
+			}
+		System.out.print(i+1 + " ");
+		}
+	}
+	
+	public static void main(String[] args) {		
+		Scanner scan = new Scanner(System.in);
+
+		int month = 0;
+		
+		while(true) {
+			System.out.println("월을 입력하세요.");
+			System.out.println(PROMPT);
+			month = scan.nextInt();
+			if(month == -1) {
+				break;
+			}
+			if(month > 12) {
+				continue;
+			}	
+			perMonthCalendar(month);
+			System.out.println("\n");
+		}
+		System.out.println("bye!");
+
+		scan.close();
+	}
+
+
+	
+
+}
