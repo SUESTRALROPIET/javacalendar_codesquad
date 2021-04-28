1bd191b12dc486e5ae66f5beffea1f14a07bff85
seungyeon <seungyeon@localhost>
2021-04-28 15:49:48 +0900

    add conditional on input variables

--- src/calendar5.java
+++ src/calendar5.java
@@ -24,7 +24,8 @@ public class  calendar5 {
 		else if(weekday.equals("THU")) return 4;
 		else if(weekday.equals("FRI")) return 5;
 		else if(weekday.equals("SAT")) return 6;
-		return 0;		
+		else
+			return 0;		
 	}
 	
 	public void printCalendar(int year, int month, String weekday) {
@@ -39,11 +40,10 @@ public class  calendar5 {
 			System.out.printf(PARSE);
 			parse++;
 		}
-		
+
 		for(int i = 1; i <= (7 - getParseDays(weekday)); i++) {
 			System.out.printf("%3d",i);
 		}
-		
 		System.out.println();
 		
 		for(int j = (7 - getParseDays(weekday)+1); j <= maxDay; j++) {
@@ -63,19 +63,36 @@ public class  calendar5 {
 		int year,month;
 		String weekday;
 		
-		System.out.println("년도를 입력하세요.");
-		System.out.printf("YEAR> ");
-		year = scan.nextInt();
-		
-		System.out.println("달을 입력하세요.");
-		System.out.printf("MONTH> ");
-		month = scan.nextInt();
-
-		System.out.println("첫번째 요일을 입력하세요.(SUN,MON,TUE,WED,THU,FRI,SAT)");
-		System.out.printf("WEEKDAY> ");
-		weekday = scan.next();
-		
-		cal.printCalendar(year, month, weekday);
+		while (true) {
+			System.out.println("년도를 입력하세요.(exit: -1)");
+			System.out.printf("YEAR> ");
+			year = scan.nextInt();
+			
+			if (year == -1) {
+				break;
+			}
+			else if (year < 1000 || year > 9999) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			
+			System.out.println("달을 입력하세요.");
+			System.out.printf("MONTH> ");
+			month = scan.nextInt();
+			
+			if(month>12 || month<1) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+	
+			System.out.println("첫번째 요일을 입력하세요.(SUN,MON,TUE,WED,THU,FRI,SAT)");
+			System.out.printf("WEEKDAY> ");
+			weekday = scan.next();
+			
+			cal.printCalendar(year, month, weekday);
+		}
+		System.out.println("Bye-");
+		scan.close();
 	}
 
 	public static void main(String[] args) {
